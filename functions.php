f311cae89e0abb9d517f3317ed13760bc4918d1f
Anne Minford <anneminford@btinternet.com>
2017-11-03 10:58:15 +0000

    Tutorial 4 -Add Bootstrap JS to WordPress Theme

--- functions.php
+++ functions.php
@@ -119,7 +119,7 @@ add_action( 'widgets_init', 'base_theme_widgets_init' );
 function base_theme_scripts() {
 	wp_enqueue_style( 'base-theme-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'base-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'base-theme-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), '1.0.0', true );
 
 	wp_enqueue_script( 'base-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
